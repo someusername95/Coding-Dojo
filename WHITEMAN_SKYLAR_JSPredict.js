e9278dfa51d3016cf69ad20685a8bfab578f1984
Skylar <skylarwhiteman95@gmail.com>
2022-05-16 16:24:36 -0600

    first commit

---
+++ WHITEMAN_SKYLAR_JSPredict.js
@@ -0,0 +1,7 @@
+//Predict 1 = syntax error? combining string and int with no concatenation?
+//Predict 2 = I was born in 1980       // no period given
+//Predict 3 = Summing numbers!
+            //num1 is: 10
+            //num2 is: 20
+            //30
+               
